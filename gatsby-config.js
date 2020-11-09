3f578213632216be999bdfb74f459159bf9cf439
Ayoub <skobraf@gmail.com>
2020-11-09 10:43:14 +0100

    initiate the app, added required packages, added prettier and eslint config

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,14 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: 'gatsby-apollo-dynamic-fetching',
+    description: 'This starter kit combine generating static pages also as fetching dynamic data from a GraphQl API using Apollo client depending on a dynamic route',
+    author: {
+      name:'Ayoub Abed',
+      username:'skobraf'
+    },
+    starterUrl:'ddddddd'
+  },
   plugins: [],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,35 @@
-import React from "react"
+import React from 'react'
+import { graphql } from 'gatsby'
 
-export default function Home() {
-  return <div>Hello world!</div>
+export default function Home({ data }) {
+  return (
+    <div>
+      <h1>This page is static, it's generated during build time</h1>
+      <h2>{data.site.siteMetadata.title}</h2>
+      <h3>{data.site.siteMetadata.description}</h3>
+      <p>
+        This Starter-kit uses{' '}
+        <a href="https://github.com/lennertVanSever/graphcountries">
+          Countries v2
+        </a>{' '}
+        public graphQl API, which enable us to fetch country data depending on
+        the country name in our case.{' '}
+      </p>
+      <br />
+      <p>
+        Nvig ate to /countries/[:nameOfTheCountry] by changing the value of
+        [:nameOfTheCountry] in the url
+      </p>
+    </div>
+  )
 }
+export const query = graphql`
+  query HomePageQuery {
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
+  }
+`
